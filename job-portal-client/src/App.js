90a976bff5b9c291c670e14ca719d4af6dda8fd6
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-11-26 12:48:03 +0600

    Basic Folder Structure :octocat:

--- job-portal-client/src/App.js
+++ job-portal-client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './Router/Routes/Routes'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Job Portal</h1>
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
