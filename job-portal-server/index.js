3ce162b3d42096ffbbf03a85ea6150ad01d9c456
Ruhul Amin Parvez <ruhul.parvez@oisbd.net>
2022-11-27 12:09:03 +0600

    setUp with MongoDB and createPost EndPoint Done :octocat:

--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.166txbz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+console.log(uri);
+
+async function run() {
+    try {
+        console.log('Connected correctly to server');
+        const db = client.db('jobPortal');
+        const collection = db.collection('jobPosts');
+        
+        app.post('/createPost', async (req, res) => {
+            const post = req.body;
+            const result = await collection.insertOne(post);
+            res.send(result);
+        });
+        
+    } finally {
+        // Ensures that the client will close when you finish/error
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
